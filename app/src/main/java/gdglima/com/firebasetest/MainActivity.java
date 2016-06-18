8bbfd8f96534d8b2e334c1133d0e527525697c90
Carlos Piñan <carlos.pinan@gmail.com>
2016-06-17 20:18:04 -0500

    Added auth and analytics.

--- app/src/main/java/gdglima/com/firebasetest/MainActivity.java
+++ app/src/main/java/gdglima/com/firebasetest/MainActivity.java
@@ -1,31 +1,63 @@
 package gdglima.com.firebasetest;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
+
     private TextView titleTextView;
     private Button btnPromotion;
+
+    private Button submitButton, signOutButton;
+    private EditText usernameEditText, passwordEditText;
+
+    private FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+            } else {
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        titleTextView = (TextView)findViewById(R.id.title);
+        setContentView(R.layout.layout_main);
+        init();
+    }
+
+    private void init() {
+        titleTextView = (TextView) findViewById(R.id.title);
         btnPromotion = (Button) findViewById(R.id.button);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        signOutButton = (Button) findViewById(R.id.signOut);
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
 
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
 
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 .setDeveloperModeEnabled(BuildConfig.DEBUG)
@@ -41,18 +73,79 @@ public class MainActivity extends AppCompatActivity {
                 loadResources();
             }
         });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = usernameEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
+                login(username, password);
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+                signOutButton.setEnabled(false);
+            }
+        });
+
     }
 
     private void loadResources() {
-
         String title = mFirebaseRemoteConfig.getString("title_example");
         titleTextView.setText(title);
-
         boolean promotionActive = mFirebaseRemoteConfig.getBoolean("promotion_on");
-
         btnPromotion.setVisibility(promotionActive ? View.VISIBLE : View.INVISIBLE);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle analyticsBundle = new Bundle();
+        analyticsBundle.putLong(FirebaseAnalytics.Param.ITEM_ID, System.currentTimeMillis());
+        mFirebaseAnalytics.logEvent(MainActivity.class.getSimpleName(), analyticsBundle);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void login(String username, String password) {
+        if (mAuth == null) {
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, R.string.authentication_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            signOutButton.setEnabled(true);
+                            Toast.makeText(MainActivity.this, R.string.authentication_success,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void logout() {
+        if (mAuth != null) {
+            mAuth.signOut();
+        }
     }
 
 }
